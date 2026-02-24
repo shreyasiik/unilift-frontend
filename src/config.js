c0953ec404503c87ae68341f2682724aa13c995c
Shreyasi <shreyasi.kathait6@gmail.com>
2026-02-24 12:02:27 +0530

    otp correction5

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_BASE =
   process.env.NODE_ENV === "production"
-    ? "https://unilift-backend.onrender.com/"
+    ? "https://unilift-backend.onrender.com".replace(/\/$/, "")
     : "http://localhost:5000";
 
 export default API_BASE;
